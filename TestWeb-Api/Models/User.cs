5195a638d704adf7546149898262b14898a84258
Владимир Кудряшов <vskudryashov_1@edu.hse.ru>
2020-02-14 16:48:23 +0300

    Модуль user допилен

--- TestWeb-Api/Models/User.cs
+++ TestWeb-Api/Models/User.cs
@@ -13,13 +13,20 @@ namespace TestWeb_Api.Models
         [Required]
         public int UserId { get; set; }
         [Required]
-        public string PhoneNumber { get; set; }
-        [Required]
         public string Name { get; set; }
         [Required]
+        public string Surname { get; set; }
+        [Required]
+        public DateTime Date_Of_Birthday { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        
+        [Required]
         public string Password { get; set; }
-        public int? FamilyId { get; set; }
+        
         public string JsonToken { get; set; }
-        public virtual Family Family { get; set; }
+        
     }
 }
